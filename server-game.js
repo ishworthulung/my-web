321ba47edbebaff82f87ba4671b50373ad1d40e2
ishworthulung <thulungraiishwor@gmail.com>
2026-01-12 10:34:18 +0545

    Add files via upload

--- server-game.js
+++ server-game.js
@@ -4,18 +4,13 @@ function spaceGameFetch(url){
   
 }
 function loadGameList(){
-    fetch('game_list.html')
+    fetch('files/res/game_list.html')
     .then(response => response.text())
     .then(data => document.getElementById('lists').innerHTML = data);
 
    
 }
-function keyPress(key){
-  let event = new KeyboardEvent('Keydown',{key: key});
-  document.dispatchEvent(event);
 
-}
-document.addEventListener('keydown', (event) => {
-  console.log(`Key pressed: ${event.key}`);
-});
+
+
 loadGameList();
